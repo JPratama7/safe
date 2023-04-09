4e7641d0ab03a6a7cf36a937c952bafbf687f0ae
JPratama7 <josepratama080@gmail.com>
2023-04-10 05:05:23 +0700

    check int and uint using zero val comparison

--- lib.go
+++ lib.go
@@ -7,7 +7,7 @@ func Checker(val reflect.Value) (res bool) {
 		return
 	}
 	switch val.Kind() {
-	case reflect.Chan, reflect.Slice, reflect.Map:
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice, reflect.UnsafePointer:
 		res = !val.IsNil()
 		break
 	case reflect.Array:
@@ -15,6 +15,13 @@ func Checker(val reflect.Value) (res bool) {
 		break
 	case reflect.String:
 		res = val != reflect.Zero(val.Type())
+		break
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		res = val != reflect.Zero(val.Type())
+		break
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		res = val != reflect.Zero(val.Type())
+		break
 	default:
 		res = !val.IsZero()
 		break
